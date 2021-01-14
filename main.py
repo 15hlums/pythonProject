d9ad59803e7419ee22db4433e4ec89e28716b0dc
Hollie <15hlums@whs.whsat.uk>
2021-01-14 11:44:17 +0000

    Some more changes

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# this is the code to create Jeff :)
+
 import pygame
 
 pygame.init()
@@ -12,9 +14,35 @@ while True:
         if event.type == pygame.QUIT:
             quit()
 
-    DISPLAYSURFACE.fill((255, 0, 0))
+    # this is the sky, Jeff's favourite colour
+    DISPLAYSURFACE.fill((0,191,255))
 
-    pygame.draw.circle(DISPLAYSURFACE, (0, 0, 0), (300, 300), 20)
+    # this is the grass Jeff likes to stand on and eat
+    pygame.draw.rect(DISPLAYSURFACE, (34,139,34), (0, 400, 800, 200), 0)
+
+    # these are his legs, he uses them to run
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (275, 410, 30, 125), 0)
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (375, 410, 30, 125), 0)
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (325, 350, 30, 150), 0)
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (425, 350, 30, 150), 0)
+
+    # this is his body, hes doesnt like body shaming
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (255, 300, 225, 125), 0)
+
+    # this is his neck, with only 7 bones in it
+    pygame.draw.rect(DISPLAYSURFACE, (255, 215, 0), (225, 100, 50, 250), 0)
+
+    # this is his head, he uses it to think
+    pygame.draw.ellipse(DISPLAYSURFACE, (255, 215, 0), (125, 50, 150, 100), 0)
+
+    # these are his eyes, he likes seeing stuff
+    pygame.draw.circle(DISPLAYSURFACE, (0, 0, 0), (200, 90), 5)
+    pygame.draw.circle(DISPLAYSURFACE, (0, 0, 0), (225, 90), 5)
+
+    # this is his mouth, nom nom
+    pygame.draw.arc(DISPLAYSURFACE, (0, 0, 0), (150, 100, 20, 20), 3, 6, 50)
+
+
 
     pygame.display.update()
 
