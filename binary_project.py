0b82b0b3bb3ceda585f460a9a803a1ace3054656
Hollie <15hlums@whs.whsat.uk>
2021-01-19 17:54:49 +0000

    Half finished 4, need to complete the decimal function of 4.

--- binary_project.py
+++ binary_project.py
@@ -262,3 +262,113 @@ def binary_converter_v3(num, comp):
         binary = add + binary
     print(binary)
     print(binary_list)
+
+def binary_converter_v4(num):
+    '''
+    If you input a 16-bit string of 1’s and 0’s,
+    it will return the two’s complement number that the string represents.
+    If you input a value outside the accepted range, it will return None.
+    If you put a value that will not perfectly fit into a binary decimal,
+    it will round the value down as little as possible.
+    It also takes a second input that tells it how many bits are used in the exponent.
+    It is assumed 1 for the sign bit and the rest for the mantissa.
+    :param: input_value a string of 1’s and 0’s
+    :param: exponent_bits: a positive integer from 1 to 13, this how many bits are used in the exponent.
+            These are always found at the end of the string
+    :return: a number
+    '''
+    if num > 1111111111111111:
+        print('None')
+    if num >= 0000000000000000:
+        binary_list = ([int(d) for d in str(num)])
+        for x in range(0, 16, 1):
+            if binary_list[x] == 1:
+                binary_list[x] = 0
+            elif binary_list[x] == 0:
+                binary_list[x] = 1
+        if binary_list[15] == 0:
+            binary_list[15] = 1
+        elif binary_list[15] == 1:
+            binary_list[15] = 0
+            if binary_list[14] == 0:
+                binary_list[14] = 1
+            elif binary_list[14] == 1:
+                binary_list[14] = 0
+                if binary_list[13] == 0:
+                    binary_list[13] = 1
+                elif binary_list[13] == 1:
+                    binary_list[13] = 0
+                    if binary_list[12] == 0:
+                        binary_list[12] = 1
+                    elif binary_list[12] == 1:
+                        binary_list[12] = 0
+                        if binary_list[11] == 0:
+                            binary_list[11] = 1
+                        elif binary_list[11] == 1:
+                            binary_list[11] = 0
+                            if binary_list[10] == 0:
+                                binary_list[10] = 1
+                            elif binary_list[10] == 1:
+                                binary_list[10] = 0
+                                if binary_list[9] == 0:
+                                    binary_list[9] = 1
+                                elif binary_list[9] == 1:
+                                    binary_list[9] = 0
+                                    if binary_list[8] == 0:
+                                        binary_list[8] = 1
+                                    elif binary_list[8] == 1:
+                                        binary_list[8] = 0
+                                        if binary_list[7] == 0:
+                                            binary_list[7] = 1
+                                        elif binary_list[7] == 1:
+                                            binary_list[7] = 0
+                                            if binary_list[6] == 0:
+                                                binary_list[6] = 1
+                                            elif binary_list[6] == 1:
+                                                binary_list[6] = 0
+                                                if binary_list[5] == 0:
+                                                    binary_list[5] = 1
+                                                elif binary_list[5] == 1:
+                                                    binary_list[5] = 0
+                                                    if binary_list[4] == 0:
+                                                        binary_list[4] = 1
+                                                    elif binary_list[4] == 1:
+                                                        binary_list[4] = 0
+                                                        if binary_list[3] == 0:
+                                                            binary_list[3] = 1
+                                                        elif binary_list[3] == 1:
+                                                            binary_list[3] = 0
+                                                            if binary_list[2] == 0:
+                                                                binary_list[2] = 1
+                                                            elif binary_list[2] == 1:
+                                                                binary_list[2] = 0
+                                                                if binary_list[1] == 0:
+                                                                    binary_list[1] = 1
+                                                                elif binary_list[1] == 1:
+                                                                    binary_list[1] = 0
+                                                                    if binary_list[0] == 0:
+                                                                        binary_list[0] = 1
+                                                                    elif binary_list[0] == 1:
+                                                                        binary_list[0] = 0
+        one_bit = ((binary_list[15]) * 2 ** 0)
+        two_bit = ((binary_list[14]) * 2 ** 1)
+        three_bit = ((binary_list[13]) * 2 ** 2)
+        four_bit = ((binary_list[12]) * 2 ** 3)
+        five_bit = ((binary_list[11]) * 2 ** 4)
+        six_bit = ((binary_list[10]) * 2 ** 5)
+        seven_bit = ((binary_list[9]) * 2 ** 6)
+        eight_bit = ((binary_list[8]) * 2 ** 7)
+        nine_bit = ((binary_list[7]) * 2 ** 8)
+        ten_bit = ((binary_list[6]) * 2 ** 9)
+        eleven_bit = ((binary_list[5]) * 2 ** 10)
+        twelve_bit = ((binary_list[4]) * 2 ** 11)
+        thirteen_bit = ((binary_list[3]) * 2 ** 12)
+        fourteen_bit = ((binary_list[2]) * 2 ** 13)
+        fifteen_bit = ((binary_list[1]) * 2 ** 14)
+        sixteen_bit = ((binary_list[0]) * 2 ** 15)
+        res = (one_bit + two_bit + three_bit + four_bit + five_bit + six_bit + seven_bit + eight_bit +
+               nine_bit + ten_bit + eleven_bit + twelve_bit + thirteen_bit + fourteen_bit + fifteen_bit +
+               sixteen_bit)
+    print(res)
+
+print(binary_converter_v4(1000000000000000))
