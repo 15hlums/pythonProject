c5e8fc45cf3d9da88eea1966ec70572ddf38c0c2
Hollie <15hlums@whs.whsat.uk>
2021-01-20 21:15:26 +0000

    Finished converter 4

--- binary_project.py
+++ binary_project.py
@@ -263,7 +263,7 @@ def binary_converter_v3(num, comp):
     print(binary)
     print(binary_list)
 
-def binary_converter_v4(num):
+def binary_converter_v4(num, exponent):
     '''
     If you input a 16-bit string of 1’s and 0’s,
     it will return the two’s complement number that the string represents.
@@ -350,25 +350,46 @@ def binary_converter_v4(num):
                                                                         binary_list[0] = 1
                                                                     elif binary_list[0] == 1:
                                                                         binary_list[0] = 0
-        one_bit = ((binary_list[15]) * 2 ** 0)
-        two_bit = ((binary_list[14]) * 2 ** 1)
-        three_bit = ((binary_list[13]) * 2 ** 2)
-        four_bit = ((binary_list[12]) * 2 ** 3)
-        five_bit = ((binary_list[11]) * 2 ** 4)
-        six_bit = ((binary_list[10]) * 2 ** 5)
-        seven_bit = ((binary_list[9]) * 2 ** 6)
-        eight_bit = ((binary_list[8]) * 2 ** 7)
-        nine_bit = ((binary_list[7]) * 2 ** 8)
-        ten_bit = ((binary_list[6]) * 2 ** 9)
-        eleven_bit = ((binary_list[5]) * 2 ** 10)
-        twelve_bit = ((binary_list[4]) * 2 ** 11)
-        thirteen_bit = ((binary_list[3]) * 2 ** 12)
-        fourteen_bit = ((binary_list[2]) * 2 ** 13)
-        fifteen_bit = ((binary_list[1]) * 2 ** 14)
-        sixteen_bit = ((binary_list[0]) * 2 ** 15)
-        res = (one_bit + two_bit + three_bit + four_bit + five_bit + six_bit + seven_bit + eight_bit +
-               nine_bit + ten_bit + eleven_bit + twelve_bit + thirteen_bit + fourteen_bit + fifteen_bit +
-               sixteen_bit)
-    print(res)
 
-print(binary_converter_v4(1000000000000000))
+
+        exponant_list = []
+        for x in range(15, 15 - exponent, -1):
+            exponant_list.append(binary_list[x])
+        exponant_list.reverse()
+
+        for x in range(15, 15 - exponent, -1):
+            binary_list.pop(x)
+
+        shift = 0
+        power = 0
+
+        for x in range(exponent - 1, -1, -1):
+            bit = ((exponant_list[x]) * 2 ** power)
+            shift += bit
+            power += 1
+
+        decimal_place = []
+
+        for x in range(15 - exponent, 15 - exponent - shift, -1):
+            decimal_place.append(binary_list[x])
+            binary_list.pop(x)
+            decimal_place.reverse()
+
+        dec_power = -1
+        dec_res = 0
+
+        for x in range(0, shift, 1):
+            dec = ((decimal_place[x]) * 2 ** dec_power)
+            dec_res += dec
+            dec_power -= 1
+
+        int_power = 0
+        int_res = 0
+
+        for x in range(len(binary_list) - 1, -1, -1):
+            integer = ((binary_list[x]) * 2 ** int_power)
+            int_res += integer
+            int_power += 1
+
+        res = int_res + dec_res
+        print(res)
